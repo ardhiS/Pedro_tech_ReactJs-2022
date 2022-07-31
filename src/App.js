5261cdc9b4d521583b5c670964eab2edf3b2f845
ardhi <ardhi333@gmail.com>
2022-07-31 11:09:38 +0700

    latihan yang dibuat sendiri tana liat video, hanya sedikit liat video ketika ada error yang tidak bisa saya selesaikan sendiri

--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
 import "./App.css";
+
 import { useState } from "react";
-import { Task } from "./Task";
 
-function App() {
-  const [nama, setNama] = useState("");
-  const [listNama, setListNama] = useState([]);
+const App = () => {
+  const [data, setData] = useState([]);
+  const [dataBaru, setDataBaru] = useState();
 
+  const tambahDataBaru = (e) => {
+    setDataBaru(e.target.value);
+  };
   const handleClick = () => {
-    const baru = {
-      id: listNama.length === 0 ? 1 : listNama[listNama.length - 1].id + 1,
-      namanya: nama,
-      completed: false,
+    const barunya = {
+      id: data.length === 0 ? 1 : data[data.length - 1].id + 1,
+      nama: dataBaru,
+      selesai: false,
     };
-    setListNama([...listNama, baru]);
+    setData([...data, barunya]);
+    setDataBaru("");
   };
 
   const hapusItem = (id) => {
-    setListNama(listNama.filter((e) => e.id !== id));
+    setData(data.filter((data) => data.id !== id));
   };
-
-  const completed = (id) => {
-    setListNama(
-      listNama.map((i) => {
-        if (i.id === id) {
-          return { ...i, completed: true };
+  const tandaiSelesai = (id) => {
+    setData(
+      data.map((item) => {
+        if (item.id === id) {
+          return { ...item, selesai: !item.selesai };
         } else {
-          return i;
+          return item;
         }
       })
     );
   };
   return (
     <div className="App">
-      <div className="name">
-        <input onChange={(e) => setNama(e.target.value)} />
-        <button onClick={handleClick}>Add</button>
+      <h1>Menambahkan Nama</h1>
+      <div className="inputan">
+        <input type="text" onChange={tambahDataBaru} value={dataBaru} />
+        <button onClick={handleClick}>Tambah Data</button>
       </div>
-      <div className="list">
-        {listNama.map((i) => {
-          return <Task key={i.id} namanya={i.namanya} id={i.id} completed={i.completed} hapusItem={hapusItem} selesai={completed} />;
-        })}
+      <div className="listnya">
+        {data.map((item) => (
+          <div key={item.id} style={{ backgroundColor: item.selesai ? "green" : "white" }}>
+            <h1>{item.nama}</h1>
+            <button onClick={() => hapusItem(item.id)}>Hapus Nama</button>
+            <button onClick={() => tandaiSelesai(item.id)}>{item.selesai ? "Bukan Nama Saya" : "Nama Saya"}</button>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
